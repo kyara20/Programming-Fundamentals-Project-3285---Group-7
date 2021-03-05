c69e6a01f594374284274d3a5dd9fb5314f6ed78
CARLA MICAELA PORTERO VALLEJO <75466024+cmportero1@users.noreply.github.com>
2021-03-04 20:51:07 -0500

    Add files via upload

---
+++ PROJECT2ValidIdfunction.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project2valididfunction;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class PROJECT2ValidIdfunction {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
